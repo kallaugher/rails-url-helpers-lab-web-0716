cf47b054cb4591c7d2af6b83962c76902c91df5e
kallaugher <alicekallaugher@gmail.com>
2016-08-17 15:32:44 -0400

    integrate linking and add activate function

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate
+    if self.active == true
+      self.active = false
+      self.save
+    else
+      self.active = true
+      self.save
+    end
+  end
 end
